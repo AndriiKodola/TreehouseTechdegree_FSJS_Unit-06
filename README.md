d6a1ce227b99cf5c1359ba0c466a073b15de3f11
Andrii Kodola <ankodola@gmail.com>
2019-02-06 23:23:09 +0100

    Exceeds content added. Style: background, colors, fonts, shadows, filters. Possible to start using npm.

---
+++ README.md
@@ -0,0 +1,8 @@
+It's a portfolio site to showcase my great projects :).
+
+Style changes:
+-backgrounds
+-fonts
+-colors
+-filters
+-shadows
--- app.js
+++ app.js
@@ -53,11 +53,6 @@ app.listen(3000, () => {
 
 /**
  * TO-DO:
- * Exceeds:
- *      Set up your package.json file so that running npm start will run the app.
- *      Change or add at least three of the following to make this project your own:
- * color, background color, font, box or text shadows, transitions or animations, add a logo
- * Your can either add your changes to the end of the CSS file or add your own and link it in the head of the layout.pug file, below the other CSS links.
  * Document your style changes in your README.md file and the project submission notes.
  * 
  * Optionally - the path module which can be used when setting the absolute path in the express.static function.
